8edf67b628cac7b2aa15c34b9f227fd2b051be0c
chienguo <2778180460@qq.com>
2025-05-10 10:58:40 +0800

    ...

---
+++ problems/p0003.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <gtest/gtest.h>
+#include <unordered_set>
+
+using namespace std;
+
+namespace {
+class Solution {
+public:
+  int lengthOfLongestSubstring(string s) {
+    auto res = 0;
+    auto left = 0;
+    auto window = unordered_set<char>();
+    for (size_t i = 0; i < s.length(); i++) {
+      while (window.find(s.at(i)) != window.end()) {
+        window.erase(s[left]);
+        left++;
+      }
+      window.insert(s.at(i));
+      res = max(res, (int)i - left + 1);
+    }
+    return res;
+  }
+};
+} // namespace
+
+TEST(p0003, c1) {
+  Solution s;
+  ASSERT_EQ(s.lengthOfLongestSubstring("abcabcbb"), 3);
+}
